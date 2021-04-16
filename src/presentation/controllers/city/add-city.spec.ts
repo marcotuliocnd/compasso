f807101fc5f8d8f9d754a67c53391f29f8429a99
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-16 18:43:51 -0300

    test: ensure AddCityController returns 400 if an invalid state is provided

--- src/presentation/controllers/city/add-city.spec.ts
+++ src/presentation/controllers/city/add-city.spec.ts
@@ -1,15 +1,24 @@
 import { AddCityController } from './add-city'
-import { MissingParamError } from './add-city-protocols'
+import { MissingParamError, InvalidParamError, StateValidator } from './add-city-protocols'
 
 interface SutTypes {
   sut: AddCityController
+  stateValidatorStub: StateValidator
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new AddCityController()
+  class StateValidatorStub implements StateValidator {
+    isValid (state: string): boolean {
+      return true
+    }
+  }
+
+  const stateValidatorStub = new StateValidatorStub()
+  const sut = new AddCityController(stateValidatorStub)
 
   return {
-    sut
+    sut,
+    stateValidatorStub
   }
 }
 
@@ -40,5 +49,22 @@ describe('AddCityController', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('state'))
+  })
+
+  test('Should return 400 if an invalid state is provided', () => {
+    const { sut, stateValidatorStub } = makeSut()
+
+    jest.spyOn(stateValidatorStub, 'isValid').mockReturnValueOnce(false)
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        state: 'invalid_state'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('state'))
   })
 })
