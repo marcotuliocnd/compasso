f281290b34994812296d605f8043b95a5f67f59d
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-16 13:10:03 -0300

    test: ensure AddCityController returns 400 if no state is provided

--- src/presentation/controllers/city/add-city.spec.ts
+++ src/presentation/controllers/city/add-city.spec.ts
@@ -14,4 +14,18 @@ describe('AddCityController', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('Should return 400 if no state is provided', () => {
+    const sut = new AddCityController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: state'))
+  })
 })
