6a4cf2fa84636dc3cf70171206329157a146f9d6
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-16 13:09:40 -0300

    feat: ensure AddCityController returns 400 if no state is provided

--- src/presentation/controllers/city/add-city.ts
+++ src/presentation/controllers/city/add-city.ts
@@ -1,8 +1,17 @@
 export class AddCityController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+
+    if (!httpRequest.body.state) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: state')
+      }
     }
   }
 }
