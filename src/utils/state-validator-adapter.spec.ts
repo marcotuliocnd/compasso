4eb5559b34c3d320d4b2dbca089653200290b167
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-17 16:23:31 -0300

    feat: move sut creation to a helper factory method

--- src/utils/state-validator-adapter.spec.ts
+++ src/utils/state-validator-adapter.spec.ts
@@ -1,14 +1,18 @@
 import { StateValidatorAdapter } from './state-validator-adapter'
 
+const makeSut = (): StateValidatorAdapter => {
+  return new StateValidatorAdapter()
+}
+
 describe('StateValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
-    const sut = new StateValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('invalid_state')
     expect(isValid).toBe(false)
   })
 
   test('Should return true if validator returns true', () => {
-    const sut = new StateValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(sut, 'validator').mockImplementationOnce(() => true)
 
     const isValid = sut.isValid('any_state')
@@ -16,7 +20,7 @@ describe('StateValidator Adapter', () => {
   })
 
   test('Should call validator with correct values', () => {
-    const sut = new StateValidatorAdapter()
+    const sut = makeSut()
     const validatorSpy = jest.spyOn(sut, 'validator').mockImplementationOnce(() => true)
 
     sut.isValid('any_state')
