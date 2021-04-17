2ebe1c2848d3f50c4403eedddd635f22f457e765
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-17 16:20:46 -0300

    test: ensure StateValidatorAdapter returns true if validator returns true

--- src/utils/state-validator-adapter.spec.ts
+++ src/utils/state-validator-adapter.spec.ts
@@ -7,9 +7,11 @@ describe('StateValidator Adapter', () => {
     expect(isValid).toBe(false)
   })
 
-  test('Should return t if validator returns false', () => {
+  test('Should return true if validator returns true', () => {
     const sut = new StateValidatorAdapter()
-    const isValid = sut.isValid('invalid_state')
-    expect(isValid).toBe(false)
+    jest.spyOn(sut, 'validator').mockImplementationOnce(() => true)
+
+    const isValid = sut.isValid('any_state')
+    expect(isValid).toBe(true)
   })
 })
