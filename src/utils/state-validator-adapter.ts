b2f92133d36ae24d024cb18ce2276800fc78a2ab
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-17 16:20:34 -0300

    feat: ensure StateValidatorAdapter returns true if validator returns true

--- src/utils/state-validator-adapter.ts
+++ src/utils/state-validator-adapter.ts
@@ -1,7 +1,13 @@
 import { StateValidator } from '../presentation/protocols/state-validator'
 
 export class StateValidatorAdapter implements StateValidator {
+  private readonly states = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RO', 'RS', 'RR', 'SC', 'SE', 'SP', 'TO']
+
+  validator (state: string): boolean {
+    return this.states.includes(state)
+  }
+
   isValid (state: string): boolean {
-    return false
+    return this.validator(state)
   }
 }
