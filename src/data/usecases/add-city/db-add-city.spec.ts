0f48deb2f655d42f25b8c72ac2c4189a0e30d622
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-22 19:35:49 -0300

    refactor: move sut creation to a helper factory method

--- src/data/usecases/add-city/db-add-city.spec.ts
+++ src/data/usecases/add-city/db-add-city.spec.ts
@@ -3,21 +3,40 @@ import { CityModel } from '../../../domain/models/city'
 import { DbAddCity } from './db-add-city'
 import { AddCityModel } from '../../../domain/usecases/city/add-city'
 
+const makeAddCityRepositoryStub = (): AddCityRepository => {
+  class AddCityRepositoryStub implements AddCityRepository {
+    async add (cityData: AddCityModel): Promise<CityModel> {
+      const fakeCity = {
+        id: 'any_id',
+        name: 'any_name',
+        state: 'any_state'
+      }
+      return await new Promise(resolve => resolve(fakeCity))
+    }
+  }
+
+  return new AddCityRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbAddCity
+  addCityRepositoryStub: AddCityRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addCityRepositoryStub = makeAddCityRepositoryStub()
+  const sut = new DbAddCity(addCityRepositoryStub)
+
+  return {
+    sut,
+    addCityRepositoryStub
+  }
+}
+
 describe('DbAddCity Usecase', () => {
   test('Should call AddCityRepository with correct values', async () => {
-    class AddCityRepositoryStub implements AddCityRepository {
-      async add (cityData: AddCityModel): Promise<CityModel> {
-        const fakeCity = {
-          id: 'any_id',
-          name: 'any_name',
-          state: 'any_state'
-        }
-        return await new Promise(resolve => resolve(fakeCity))
-      }
-    }
+    const { sut, addCityRepositoryStub } = makeSut()
 
-    const addCityRepositoryStub = new AddCityRepositoryStub()
-    const sut = new DbAddCity(addCityRepositoryStub)
     const addSpy = jest.spyOn(addCityRepositoryStub, 'add')
     const cityData = {
       name: 'valid_name',
