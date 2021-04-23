46a3bbd6fef6aa0485a211a46d85fc3855de7a1c
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-22 22:23:08 -0300

    test: ensure ListCitiesController returns 500 if ListCities throws

--- src/presentation/controllers/city/list-cities/list-cities.spec.ts
+++ src/presentation/controllers/city/list-cities/list-cities.spec.ts
@@ -1,5 +1,5 @@
 import { ListCitiesController } from './list-cities'
-import { CityModel, ListCities, ok } from './list-cities-protocols'
+import { CityModel, ListCities, ok, serverError } from './list-cities-protocols'
 
 const makeFakeCities = (): CityModel[] => {
   return [{
@@ -59,5 +59,12 @@ describe('ListCity Controller', () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(ok(makeFakeCities()))
+  })
+
+  test('Sould return 500 if ListCities throws', async () => {
+    const { sut, listCitiesStub } = makeSut()
+    jest.spyOn(listCitiesStub, 'list').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(serverError())
   })
 })
