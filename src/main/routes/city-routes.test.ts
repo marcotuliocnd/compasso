9d4dd34e3ee98f8500ab6d23ab4036223ddf9329
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-22 21:04:01 -0300

    refactor: connect to mongodb before integration test

--- src/main/routes/city-routes.test.ts
+++ src/main/routes/city-routes.test.ts
@@ -1,7 +1,21 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('City Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const cityCollection = MongoHelper.getCollection('cities')
+    await cityCollection.deleteMany({})
+  })
+
   test('Should return a city on success', async () => {
     await request(app)
       .post('/v1/cities')
