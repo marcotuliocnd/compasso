8de2adaea86ff6f0255780cab681236a6dd31cb5
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-26 12:41:53 -0300

    test: ensure AddCustomerController returns 400 if no age is provided

--- src/presentation/controllers/customer/add-customer/add-customer.spec.ts
+++ src/presentation/controllers/customer/add-customer/add-customer.spec.ts
@@ -49,4 +49,22 @@ describe('AddCustomerController', () => {
       error: new MissingParamError('gender').message
     })
   })
+
+  test('Should return 400 if no age is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any_name',
+        gender: 'any_gender',
+        birthdate_at: 'any_date',
+        city: 'any_city'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual({
+      error: new MissingParamError('age').message
+    })
+  })
 })
