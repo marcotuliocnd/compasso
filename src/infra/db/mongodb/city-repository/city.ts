ff7866c85d293c7bd7e5cc5126d94a18c4cc6043
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-26 10:40:28 -0300

    feat: ensure CityMongoRepository list all cities

--- src/infra/db/mongodb/city-repository/city.ts
+++ src/infra/db/mongodb/city-repository/city.ts
@@ -1,13 +1,21 @@
 import { CityModel } from '../../../../domain/models/city'
 import { AddCityModel } from '../../../../domain/usecases/city/add-city'
 import { AddCityRepository } from '../../../../data/protocols/add-city-repository'
+import { ListCitiesRepository } from '../../../../data/protocols/list-cities-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class CityMongoRepository implements AddCityRepository {
+export class CityMongoRepository implements AddCityRepository, ListCitiesRepository {
   async add (cityData: AddCityModel): Promise<CityModel> {
     const cityCollection = MongoHelper.getCollection('cities')
 
     const result = await cityCollection.insertOne(cityData)
     return MongoHelper.map(result.ops[0])
+  }
+
+  async list (params = {}): Promise<CityModel[]> {
+    const cityCollection = MongoHelper.getCollection('cities')
+
+    const cities: CityModel[] = await cityCollection.find(params).toArray()
+    return cities.map(el => MongoHelper.map(el))
   }
 }
