c9ade9b69f2e388a866c41d3e3e6f4f780ea4fa5
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-26 14:47:07 -0300

    feat: ensure AddCustomerController returns 500 if FindOneCity throws

--- src/presentation/controllers/customer/add-customer/add-customer.ts
+++ src/presentation/controllers/customer/add-customer/add-customer.ts
@@ -1,6 +1,6 @@
 import { FindOneCity } from './../../../../domain/usecases/city/find-one-city'
 import { MissingParamError } from './../../../errors/missing-param-errors'
-import { badRequest } from './../../../helpers/http-helper'
+import { badRequest, serverError } from './../../../helpers/http-helper'
 import { HttpRequest, HttpResponse } from './../../../protocols/http'
 import { Controller } from './../../../protocols/controller'
 
@@ -8,21 +8,25 @@ export class AddCustomerController implements Controller {
   constructor (private readonly findOneCity: FindOneCity) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['name', 'gender', 'age', 'birthdate_at', 'city']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['name', 'gender', 'age', 'birthdate_at', 'city']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
 
-    const { city } = httpRequest.body
-    await this.findOneCity.findBy({
-      id: city
-    })
+      const { city } = httpRequest.body
+      await this.findOneCity.findBy({
+        id: city
+      })
 
-    return {
-      statusCode: 200,
-      body: null
+      return {
+        statusCode: 200,
+        body: null
+      }
+    } catch (error) {
+      return serverError()
     }
   }
 }
