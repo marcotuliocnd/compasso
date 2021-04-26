673101b837d1891bcc6497f2de948e9dffd01887
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-26 16:13:53 -0300

    test: ensure DbAddCustomer calls AddCustomerRepository with correct values

---
+++ src/data/usecases/add-customer/db-add-customer.spec.ts
@@ -0,0 +1,60 @@
+import { CustomerModel, AddCustomerModel, AddCustomerRepository, DbAddCustomer } from './db-add-customer-protocols'
+
+const makeAddCustomerRepository = (): AddCustomerRepository => {
+  class AddCustomerRepositoryStub implements AddCustomerRepository {
+    async add (customerData: AddCustomerModel): Promise<CustomerModel> {
+      const fakeCustomer: CustomerModel = {
+        id: 'any_id',
+        age: 'any_age',
+        birthdate_at: 'any_date',
+        city: 'any_city_id',
+        gender: 'any_gender',
+        name: 'any_name'
+      }
+
+      return await new Promise(resolve => resolve(fakeCustomer))
+    }
+  }
+
+  return new AddCustomerRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbAddCustomer
+  addCustomerRepositoryStub: AddCustomerRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addCustomerRepositoryStub = makeAddCustomerRepository()
+  const sut = new DbAddCustomer(addCustomerRepositoryStub)
+  return {
+    sut,
+    addCustomerRepositoryStub
+  }
+}
+
+describe('DbAddCustomer Usecase', () => {
+  test('Shoud call AddCustomerRepository with correct values', async () => {
+    const { sut, addCustomerRepositoryStub } = makeSut()
+
+    const addSpy = jest.spyOn(addCustomerRepositoryStub, 'add')
+
+    const customerData: AddCustomerModel = {
+      age: 'any_age',
+      birthdate_at: 'any_date',
+      city: 'any_city_id',
+      gender: 'any_gender',
+      name: 'any_name'
+    }
+
+    await sut.add(customerData)
+
+    expect(addSpy).toHaveBeenCalledWith({
+      age: 'any_age',
+      birthdate_at: 'any_date',
+      city: 'any_city_id',
+      gender: 'any_gender',
+      name: 'any_name'
+    })
+  })
+})
