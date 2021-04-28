902cbf31d337fc4155c23c40e97821f0d77a474d
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-27 21:01:21 -0300

    test: add DeleteCustomer route

--- src/main/routes/customer-routes.test.ts
+++ src/main/routes/customer-routes.test.ts
@@ -68,4 +68,36 @@ describe('City Routes', () => {
         .expect(200)
     })
   })
+
+  describe('DeleteCustomer', () => {
+    test('Should return delete customer on success', async () => {
+      const cityCollection = MongoHelper.getCollection('cities')
+      const customerCollection = MongoHelper.getCollection('customers')
+
+      const { ops } = await cityCollection.insertOne({
+        name: 'Uberlandia',
+        state: 'MG'
+      })
+
+      const city = MongoHelper.map(ops[0])
+
+      const { ops: customerOps } = await customerCollection.insertOne({
+        name: 'Marco Tulio',
+        age: '20',
+        birthdate_at: '2000-09-11',
+        city: String(city.id),
+        gender: 'masculino'
+      })
+
+      const customer: CustomerModel = MongoHelper.map(customerOps[0])
+
+      await request(app)
+        .delete(`/v1/customers/${customer.id}`)
+        .expect(200)
+
+      const total = await customerCollection.find().count()
+
+      expect(total).toBe(0)
+    })
+  })
 })
