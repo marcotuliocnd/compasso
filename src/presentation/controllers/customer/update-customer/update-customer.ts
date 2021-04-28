e9d2db38bb82404ba6e050642e73fabc5ce2e43b
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-27 23:04:40 -0300

    feat: ensure UpdateCustomerController returns 404 if an invalid city is provided

--- src/presentation/controllers/customer/update-customer/update-customer.ts
+++ src/presentation/controllers/customer/update-customer/update-customer.ts
@@ -1,6 +1,7 @@
+import { InvalidParamError } from './../../../errors/invalid-param-errors'
 import { FindOneCity } from './../../../../domain/usecases/city/find-one-city'
 import { MissingParamError } from './../../../errors/missing-param-errors'
-import { badRequest } from './../../../helpers/http-helper'
+import { badRequest, notFound } from './../../../helpers/http-helper'
 import { HttpRequest, HttpResponse } from './../../../protocols/http'
 import { Controller } from './../../../protocols/controller'
 import { UpdateCustomerById } from './../../../../domain/usecases/customer/update-customer-by-id'
@@ -10,8 +11,13 @@ export class UpdateCustomerController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (httpRequest.body.city) {
-      await this.findOneCity.findBy(httpRequest.body.city)
+      const cityFound = await this.findOneCity.findBy(httpRequest.body.city)
+
+      if (!cityFound) {
+        return notFound(new InvalidParamError('city'))
+      }
     }
+
     await this.updateCustomerById.update(httpRequest.params.customerId, httpRequest.body)
 
     return badRequest(new MissingParamError('customerId'))
