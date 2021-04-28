f49eda03a65a5bdad6b7c53d86b0ead2f554f6d4
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-27 22:58:41 -0300

    feat: ensure FindOneCity is called with correct values

--- src/presentation/controllers/customer/update-customer/update-customer.ts
+++ src/presentation/controllers/customer/update-customer/update-customer.ts
@@ -1,3 +1,4 @@
+import { FindOneCity } from './../../../../domain/usecases/city/find-one-city'
 import { MissingParamError } from './../../../errors/missing-param-errors'
 import { badRequest } from './../../../helpers/http-helper'
 import { HttpRequest, HttpResponse } from './../../../protocols/http'
@@ -5,9 +6,10 @@ import { Controller } from './../../../protocols/controller'
 import { UpdateCustomerById } from './../../../../domain/usecases/customer/update-customer-by-id'
 
 export class UpdateCustomerController implements Controller {
-  constructor (private readonly updateCustomerById: UpdateCustomerById) {}
+  constructor (private readonly updateCustomerById: UpdateCustomerById, private readonly findOneCity: FindOneCity) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.findOneCity.findBy(httpRequest.body.city)
     await this.updateCustomerById.update(httpRequest.params.customerId, httpRequest.body)
 
     return badRequest(new MissingParamError('customerId'))
