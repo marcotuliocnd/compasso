a1b8e0d29d8bb245f3011349ddb08456a839b979
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-27 23:12:12 -0300

    feat: ensure UpdateCustomerController returns 500 if FindOneCity throws

--- src/presentation/controllers/customer/update-customer/update-customer.ts
+++ src/presentation/controllers/customer/update-customer/update-customer.ts
@@ -1,7 +1,7 @@
 import { InvalidParamError } from './../../../errors/invalid-param-errors'
 import { FindOneCity } from './../../../../domain/usecases/city/find-one-city'
 import { MissingParamError } from './../../../errors/missing-param-errors'
-import { badRequest, notFound } from './../../../helpers/http-helper'
+import { badRequest, notFound, serverError } from './../../../helpers/http-helper'
 import { HttpRequest, HttpResponse } from './../../../protocols/http'
 import { Controller } from './../../../protocols/controller'
 import { UpdateCustomerById } from './../../../../domain/usecases/customer/update-customer-by-id'
@@ -10,18 +10,22 @@ export class UpdateCustomerController implements Controller {
   constructor (private readonly updateCustomerById: UpdateCustomerById, private readonly findOneCity: FindOneCity) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { id, ...updateParams } = httpRequest.body
+    try {
+      const { id, ...updateParams } = httpRequest.body
 
-    if (updateParams.city) {
-      const cityFound = await this.findOneCity.findBy(updateParams.city)
+      if (updateParams.city) {
+        const cityFound = await this.findOneCity.findBy(updateParams.city)
 
-      if (!cityFound) {
-        return notFound(new InvalidParamError('city'))
+        if (!cityFound) {
+          return notFound(new InvalidParamError('city'))
+        }
       }
+
+      await this.updateCustomerById.update(httpRequest.params.customerId, updateParams)
+
+      return badRequest(new MissingParamError('customerId'))
+    } catch (error) {
+      return serverError()
     }
-
-    await this.updateCustomerById.update(httpRequest.params.customerId, updateParams)
-
-    return badRequest(new MissingParamError('customerId'))
   }
 }
