766c91b09ff06d6426888d3e4ff7d66d5b477769
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-27 23:50:39 -0300

    feat: ensure CustomerMongoRepository.update returns an updated customer on success

--- src/infra/db/mongodb/customer-repository/customer.ts
+++ src/infra/db/mongodb/customer-repository/customer.ts
@@ -1,3 +1,5 @@
+import { UpdateCustomerByIdModel } from './../../../../domain/usecases/customer/update-customer-by-id'
+import { UpdateCustomerByIdRepository } from './../../../../data/protocols/update-customer-by-id-repository'
 import { ListCustomerRepository } from './../../../../data/protocols/list-customer-repository'
 import { ListCustomerModel } from './../../../../domain/usecases/customer/list-customer'
 import { FindCustomerByIdRepository } from './../../../../data/protocols/find-customer-by-id-repository'
@@ -6,7 +8,7 @@ import { CustomerModel } from './../../../../domain/models/customer'
 import { AddCustomerModel } from './../../../../domain/usecases/customer/add-customer'
 import { AddCustomerRepository } from './../../../../data/protocols/add-customer-repository'
 
-export class CustomerMongoRepository implements AddCustomerRepository, FindCustomerByIdRepository, ListCustomerRepository {
+export class CustomerMongoRepository implements AddCustomerRepository, FindCustomerByIdRepository, ListCustomerRepository, UpdateCustomerByIdRepository {
   async add (customerData: AddCustomerModel): Promise<CustomerModel> {
     const customerCollection = MongoHelper.getCollection('customers')
 
@@ -39,5 +41,20 @@ export class CustomerMongoRepository implements AddCustomerRepository, FindCusto
     const customers: CustomerModel[] = await customerCollection.find(params).toArray()
 
     return customers.map(el => MongoHelper.map(el))
+  }
+
+  async update (id: string, params: UpdateCustomerByIdModel): Promise<CustomerModel> {
+    const customerCollection = MongoHelper.getCollection('customers')
+    const objectId = MongoHelper.parseObjectId(id)
+    const updated = await customerCollection.updateOne({ _id: objectId }, {
+      $set: params
+    })
+    console.log(updated.upsertedId)
+
+    const customer: CustomerModel | null = await customerCollection.findOne({
+      _id: objectId
+    })
+
+    return customer ? MongoHelper.map(customer) : null
   }
 }
