8ceb49359f2b3a88bcdf772e6b4b5cb905e61637
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-27 23:41:26 -0300

    feat: ensure DbUpdateCustomer returns an updated customer um success

--- src/data/usecases/update-customer-by-id/db-update-customer-by-id.ts
+++ src/data/usecases/update-customer-by-id/db-update-customer-by-id.ts
@@ -6,15 +6,8 @@ export class DbUpdateCustomerById implements UpdateCustomerById {
   constructor (private readonly updateCustomerByIdRepository: UpdateCustomerByIdRepository) {}
 
   async update (id: string, values: UpdateCustomerByIdModel): Promise<CustomerModel> {
-    await this.updateCustomerByIdRepository.update(id, values)
+    const updatedCustomer = await this.updateCustomerByIdRepository.update(id, values)
 
-    return {
-      id: 'any_id',
-      name: 'any_name',
-      age: 'any_age',
-      birthdate_at: 'any_date',
-      city: 'any_city',
-      gender: 'any_gender'
-    }
+    return updatedCustomer
   }
 }
