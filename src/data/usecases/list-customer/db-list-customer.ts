2a44a6c3affcac16c924c02ae3fbb093f51274f5
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-04-27 21:44:36 -0300

    feat: ensure DbListCustomer returns a list of customers on success

--- src/data/usecases/list-customer/db-list-customer.ts
+++ src/data/usecases/list-customer/db-list-customer.ts
@@ -6,22 +6,8 @@ export class DbListCustomer implements ListCustomer {
   constructor (private readonly listCustomerRepository: ListCustomerRepository) {}
 
   async list (params: ListCustomerModel = {}): Promise<CustomerModel[]> {
-    await this.listCustomerRepository.list(params)
+    const customers = await this.listCustomerRepository.list(params)
 
-    return [{
-      id: 'any_id',
-      age: 'any_age',
-      birthdate_at: 'any_date',
-      city: 'any_city',
-      gender: 'any_gender',
-      name: 'any_name'
-    }, {
-      id: 'other_id',
-      age: 'other_age',
-      birthdate_at: 'other_date',
-      city: 'other_city',
-      gender: 'other_gender',
-      name: 'other_name'
-    }]
+    return customers
   }
 }
